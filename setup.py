83072112284919655f4f6a26719838621323d7b5
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2020-01-18 16:10:26 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="qcc",
+    version="0.0.1",
+    author="Jonathan Buchanan",
+    author_email="jonathan.russ.buchanan@gmail.com",
+    description="Simple C/C++ build tool",
+    scripts=["qcc"]
+)
